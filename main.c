86aa752f4867de14fa19dbf21f2ee5e2f72c7e67
mariverg <mariverg@student.42malaga.com>
2025-01-16 12:13:41 +0100

    historial

--- main.c
+++ main.c
@@ -2,30 +2,32 @@
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: ggomez-m <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
+/*                                                    +:+ +:+
+	+:+     */
+/*   By: ggomez-m <marvin@42.fr>                    +#+  +:+
+	+#+        */
+/*                                                +#+#+#+#+#+
+	+#+           */
 /*   Created: 2025/01/08 12:05:33 by ggomez-m          #+#    #+#             */
 /*   Updated: 2025/01/08 12:05:59 by ggomez-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
-#include <stdio.h>
-#include <readline/readline.h>
-#include <readline/history.h>
-#include <sys/wait.h>
-
-#include "minishell.h"
-#include "parseo/parseo.h"
 #include "history/history.h"
 #include "libs/libft/libft.h"
+#include "minishell.h"
+#include "parseo/parseo.h"
 #include <fcntl.h>
+#include <readline/history.h>
+#include <readline/readline.h>
+#include <stdio.h>
+#include <sys/wait.h>
 
-void prntpwdline(t_env *te)
+
+/* void prntpwdline(t_env *te)
 {
 	char	*directorio;
-	
+
 	actualicepwd(te);
 	directorio = getmienv(te, "PWD");
 	if(directorio)
@@ -33,37 +35,58 @@ void prntpwdline(t_env *te)
 		write(STDOUT_FILENO, directorio, ft_strlen(directorio));
 		free (directorio);
 	}
-}
+} */
 
-int main(int argc, char **argv, char **argenv)
+char	*prntpwdline(t_env *te)
 {
-    t_env       *te;
-    char        *input;
-    t_command   *commands;
-    t_task      *tc;
+	char *directorio;
+	char *prompt;
+	size_t dir_len;
 
-
-    blockaction();
-    te = newenv(argenv);
-    init_history(); // Inicializa el historial
-	// init_readline_customization();
-    while (1)
-    {
-        prntpwdline(te);
-        input = readline(">");
-        if (!input)
-            break;
-        add_to_history(input); // Agrega el comando al historial
-        input = expanddollars(te, input);
-        commands = parse(input);
-        tc = dotaskslist(commands, te);
-		/// aqui he pasado todos los nuevos procesos a una nueva carpeta, taskparser para que funcionen cosas como cat | cat | ls, he cambiado sustancialmente la estructura de procesos inittp en la carpeta taskparset, funcion inicio taskparser
-		inittp(tc);
-        free(input);
-    }
-    save_history_to_file(); // Guarda el historial en un archivo
-    freeenv(te);
-    return (0);
+	actualicepwd(te);
+	directorio = getmienv(te, "PWD");
+	if (directorio)
+	{
+		dir_len = ft_strlen(directorio);//calculo la longuitud del directorio
+		prompt = malloc(dir_len + 4);// Espacio para el directorio + " > "
+		if (!prompt)
+		{
+			free(directorio);
+			return (strdup("error_generating_prompt > "));
+		}
+		ft_memcpy(prompt, directorio, dir_len);//copio el directorio
+		prompt[dir_len] = ' ';
+		prompt[dir_len + 1] = '>';
+		prompt[dir_len + 2] = ' ';
+		prompt[dir_len + 3] = '\0';
+		free(directorio);// Libero la memoria del directorio
+		return (prompt);
+	}
+	return (ft_strdup("unknown_directory > "));
 }
 
+int	main(int argc, char **argv, char **argenv)
+{
+	t_env *te;
+	char *input;
+	t_command *commands;
+	t_task *tc;
 
+	blockaction();
+	te = newenv(argenv);
+	while (1)
+	{
+		input = readline(prntpwdline(te));//Obtengo el prompt directamente dentro de readline
+		if (!input)
+			break ;
+
+		if (*input) // Si no está vacío, agrégo al historial
+			add_history(input);
+
+		input = expanddollars(te, input);
+		commands = parse(input);
+		tc = dotaskslist(commands, te);
+		inittp(tc);
+		free(input);
+	}
+}
