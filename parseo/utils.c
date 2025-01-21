9804dee06fd9e15106909be75a8bd5af6640b9f2
mariverg <mariverg@student.42malaga.com>
2025-01-21 14:22:53 +0100

    arreglo makefile

--- parseo/utils.c
+++ parseo/utils.c
@@ -6,7 +6,7 @@
 /*   By: mariverg <mariverg@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/09 10:58:20 by mariverg          #+#    #+#             */
-/*   Updated: 2025/01/09 11:05:28 by mariverg         ###   ########.fr       */
+/*   Updated: 2025/01/21 14:17:18 by mariverg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -98,52 +98,3 @@ char	**add_argument(char **args, char *new_arg)
 	return (new_args);
 }
 
-void	free_tokens(t_token *tokens)
-{
-	t_token	*temp;
-
-	while (tokens)
-	{
-		temp = tokens;
-		tokens = tokens->next;
-		free(temp->value);
-		free(temp);
-	}
-}
-
-void	free_commands(t_command *commands)
-{
-	t_command	*temp;
-	int			i;
-
-	while (commands)
-	{
-		temp = commands;
-		commands = commands->next;
-		if (temp->args)
-		{
-			i = 0;
-			while (temp->args[i])
-				free(temp->args[i++]);
-			free(temp->args);
-		}
-		free(temp->infile);
-		free(temp->outfile);
-		free(temp);
-	}
-}
-
-bool	is_space(char c)
-{
-	return (c == ' ' || c == '\t' || c == '\n');
-}
-
-bool	is_operator(char c)
-{
-	return (c == '|' || c == '<' || c == '>');
-}
-
-bool	is_quote(char c)
-{
-	return (c == '\'' || c == '"');
-}
